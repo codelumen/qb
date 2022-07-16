9f160abb4c6cf91eeec1cd73b03c533249df0c6f
anore <66754841+codelumen@users.noreply.github.com>
2022-07-16 03:47:38 +0300

    readonly flags, added in method, fixed hasPermission

--- src/Role.ts
+++ src/Role.ts
@@ -1,23 +1,29 @@
+import { Guild } from "discord.js";
 import Permission from "./Permission";
 
 
 export interface IRole {
-    name: string,
-    weight: number,
-    permissions: Permission[],
-    inherits?: Role[];
+    readonly name: string,
+    readonly weight: number,
+    readonly permissions: Permission[],
+    readonly inherits?: Role[];
 }
 
 
 export default class Role implements IRole {
-    public name: string;
-    public weight: number;
-    public permissions: Permission[];
-    public inherits: Role[];
+    public readonly name: string;
+    public readonly weight: number;
+    public readonly permissions: Permission[] = [];
+    public readonly inherits: Role[] = [];
 
-    public hasPermission(id: string) {
-        let inheritedPermissions = this.inherits.map(r => r.permissions).reduce((a, b) => a.concat(b));
-        return this.permissions.concat(inheritedPermissions).some(permission => permission.id === id);
+    public hasPermission(permission: Permission) {
+        return this.permissions.concat(
+            this.inherits ? this.inherits.map(r => r.permissions).reduce((a, b) => a.concat(b)) : []
+        ).some(p => p.id === permission.id);
+    }
+
+    public in(guild: Guild) {
+        return guild.roles.cache.find(r => r.name === this.name);
     }
 
     constructor({ name, weight, permissions, inherits }: IRole) {

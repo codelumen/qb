d1beb4901ab63a02f533b3b5a7680087e884f8b1
anore <66754841+codelumen@users.noreply.github.com>
2022-07-16 03:48:15 +0300

    added more colors

--- src/structures/commands/embed.ts
+++ src/structures/commands/embed.ts
@@ -4,8 +4,8 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 export default new SlashCommandBuilder()
     .setName('embed')
     .setDescription('Отправляет декоративное сообщение в определенный канал. (Используйте \\n для многострочного текста)')
-    .addStringOption(option => option.setName('описание').setDescription('Текст эмбеда.').setRequired(true))
-    .addChannelOption(option => option.setName('канал').setDescription('Канал, куда будет отправлено сообщение.').setRequired(true))
+    .addStringOption(option => option.setName('текст').setDescription('Текст эмбеда.').setRequired(true))
+    .addChannelOption(option => option.setName('канал').setDescription('Канал, куда будет отправлено сообщение.'))
     .addStringOption(option => option.setName('заголовок').setDescription('Заголовок эмбеда.'))
     .addAttachmentOption(option => option.setName('изображение').setDescription('Изображение в эмбеде.'))
     .addBooleanOption(option => option.setName('авторство').setDescription('Отображение автора эмбеда.'))
@@ -13,6 +13,9 @@ export default new SlashCommandBuilder()
         { name: 'Прозрачный', value: '#2F3136' },
         { name: 'Белый', value: '#FFFFFF' },
         { name: 'Черный', value: '#000000' },
+        { name: 'Красный', value: '#CD2626' },
+        { name: 'Жёлтый', value: '#E0AC25' },
+        { name: 'Зелёный', value: '#26CD2D' },
         { name: 'Акцентный(феолетовый)', value: '#3A41E4' }
     ))
     .addStringOption(option => option.setName('редактировать').setDescription('Редактировать уже имеющееся сообщение. (ID)'));

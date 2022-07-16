152fb2c9ece22014848fc551854a34c540659bd5
anore <66754841+codelumen@users.noreply.github.com>
2022-07-16 03:48:23 +0300

    commands list extended

--- src/structures/commands/index.ts
+++ src/structures/commands/index.ts
@@ -1,8 +1,12 @@
 import Health from './health';
 import Embed from './embed';
+import Test from './test';
+import Moderation from './moderation';
 
 
 export default [
     Health,
-    Embed
+    Embed,
+    Test,
+    ...Moderation
 ];

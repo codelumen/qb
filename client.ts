b0aa7e7d906ddc15807df91f6148135ad0f5bec3
anore <66754841+codelumen@users.noreply.github.com>
2022-07-16 03:42:09 +0300

    extended intents and partials

--- client.ts
+++ client.ts
@@ -6,7 +6,9 @@ export const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.DIRECT_MESSAGES
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
     ],
-    partials: [ 'CHANNEL' ]
+    partials: [ 'MESSAGE', 'CHANNEL', 'REACTION' ]
 });

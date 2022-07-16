db3ae3c5c90fba990ce9b5c82b09432342153e8d
anore <66754841+codelumen@users.noreply.github.com>
2022-07-16 03:44:14 +0300

    adapted to new event type, refactored

--- src/controllers/interaction/command/embed.ts
+++ src/controllers/interaction/command/embed.ts
@@ -1,41 +1,43 @@
 import { CacheType, ColorResolvable, CommandInteraction, GuildTextBasedChannel, MessageEmbed } from "discord.js";
 import { CommandController } from "../../../CommandController";
+import Embed from "../../../Embed";
 import Permission from "../../../Permission";
+import ReplyableEvent from "../../../ReplyableEvent";
 import embed from "../../../structures/permissions/editing/embed";
-import { failMessage, successMessage } from "../../../utils/messageWrapper";
 
 
 export class EmbedController extends CommandController {
     public name = 'embed';
     public perm: Permission = embed;
 
-    public async callback(interaction: CommandInteraction<CacheType>) {
+    public async callback(interaction: ReplyableEvent<CommandInteraction<CacheType>>) {
         let embed = new MessageEmbed()
-            .setDescription(interaction.options.getString('описание', true));
+            .setDescription(interaction.event.options.getString('текст', true).replaceAll('\\n', '\n'));
 
-        let title = interaction.options.getString('заголовок');
-        let image = interaction.options.getAttachment('изображение');
-        let color = interaction.options.getString('цвет');
-        let editId = interaction.options.getString('редактировать');
+        let channelId = interaction.event.options.getChannel('канал')?.id;
+        let title = interaction.event.options.getString('заголовок');
+        let image = interaction.event.options.getAttachment('изображение');
+        let color = interaction.event.options.getString('цвет');
+        let editId = interaction.event.options.getString('редактировать');
 
         if (title) embed.setTitle(title);
         if (image) embed.setImage(image.url);
         if (color) embed.setColor(color as ColorResolvable);
-        if (interaction.options.getBoolean('авторство')) {
+        if (interaction.event.options.getBoolean('авторство')) {
             embed.setFooter({
                 text: `${interaction.member.user.username}#${interaction.member.user.discriminator}`,
                 iconURL: `https://cdn.discordapp.com/avatars/${interaction.member.user.id}/${interaction.member.user.avatar}.png`
             });
         }
 
-        let targetChannel = await interaction.guild.channels.fetch(interaction.options.getChannel('канал').id);
+        let targetChannel = channelId ? await interaction.guild.channels.fetch(channelId) : interaction.event.channel;
         if (targetChannel.isText) {
             if (editId) {
                 try {
                     let message = await (targetChannel as GuildTextBasedChannel).messages.fetch(editId);
                     await message.edit({ embeds: [ embed ] });
                 } catch(e) {
-                    await interaction.reply(failMessage('Данное сообщение не найдено. Убедитесь, что указан верный канал и идентификатор.'));
+                    await interaction.reply(Embed.fail('Данное сообщение не найдено. Убедитесь, что указан верный канал и идентификатор.'));
                     return;
                 }
             } else {
@@ -43,6 +45,6 @@ export class EmbedController extends CommandController {
             }
         }
 
-        await interaction.reply(successMessage('Сообщение создано/отредактировано.'));
+        await interaction.reply(Embed.success('Сообщение создано/отредактировано.', null, true));
     }
 }

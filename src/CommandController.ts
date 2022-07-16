7c5ef72823f4d9f1ac1819cee15e7049354f2e45
anore <66754841+codelumen@users.noreply.github.com>
2022-07-16 03:43:31 +0300

    readonly flags, updated event type, fixed logic

--- src/CommandController.ts
+++ src/CommandController.ts
@@ -1,25 +1,29 @@
 import { CacheType, CommandInteraction } from 'discord.js';
 import { Controller, Endpoint } from './Controller';
+import Embed from './Embed';
 import Permission from './Permission';
+import ReplyableEvent from './ReplyableEvent';
 import RoleService from './RoleService';
-import { failMessage } from './utils/messageWrapper';
 
 
 @Endpoint
-export class CommandController extends Controller<CommandInteraction<CacheType>> {
-    public name: string;
-    public perm: Permission;
+export class CommandController extends Controller<ReplyableEvent<CommandInteraction<CacheType>>> {
+    public readonly name: string;
+    public readonly perm: Permission;
 
-    public async validation(interaction: CommandInteraction<CacheType>): Promise<boolean> {
-        return interaction.commandName === this.name;
+    public async validation(interaction: ReplyableEvent<CommandInteraction<CacheType>>): Promise<boolean> {
+        return interaction.event.commandName === this.name;
     }
 
-    public async permission(interaction: CommandInteraction<CacheType>): Promise<boolean> {
-        let has = await RoleService.hasPermission(interaction.member, this.perm.id, interaction.guild);
-        return has;
+    public async permission(interaction: ReplyableEvent<CommandInteraction<CacheType>>): Promise<boolean> {
+        if (this.perm) {
+            let has = await interaction.hasPermission(this.perm);
+            return has;
+        }
+        return true;
     }
 
-    public async denied(interaction: CommandInteraction<CacheType>) {
-        await interaction.reply(failMessage(`Недостаточно прав для использования данной команды. (${this.perm.id})`, null, true));
+    public async denied(interaction: ReplyableEvent<CommandInteraction<CacheType>>) {
+        await interaction.reply(Embed.fail(`Недостаточно прав для использования данной команды. (${this.perm.id})`, null, true));
     }
 }

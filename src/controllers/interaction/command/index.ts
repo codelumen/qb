708858bfa31abcb1752e974f312bd96b1beecd80
anore <66754841+codelumen@users.noreply.github.com>
2022-07-16 03:44:54 +0300

    extended controllers list

--- src/controllers/interaction/command/index.ts
+++ src/controllers/interaction/command/index.ts
@@ -2,13 +2,18 @@ import { CommandInteraction, CacheType } from "discord.js";
 import { Controller, ControllerConstructor } from "../../../Controller";
 import { HealthController } from './health';
 import { EmbedController } from './embed';
+import { TestController } from "./test";
+import ReplyableEvent from '../../../ReplyableEvent';
+import moderation from "./moderation";
 
 
 export default new (
-    class CommandInteractionController extends Controller<CommandInteraction<CacheType>> {
-        public static nested: ControllerConstructor<CommandInteraction<CacheType>>[] = [
+    class CommandInteractionController extends Controller<ReplyableEvent<CommandInteraction<CacheType>>> {
+        public static nested: ControllerConstructor<ReplyableEvent<CommandInteraction<CacheType>>>[] = [
             HealthController,
-            EmbedController
+            EmbedController,
+            TestController,
+            ...moderation
         ];
 
         public get nested() { return CommandInteractionController.nested }

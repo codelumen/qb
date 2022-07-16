3ea5dd741c4cee24d086a7f81a9f42f18595e65a
anore <66754841+codelumen@users.noreply.github.com>
2022-07-16 03:41:52 +0300

    added messageReactionAdd, messageReactionRemove event handling

--- bot.ts
+++ bot.ts
@@ -4,7 +4,11 @@ import { client } from './client';
 import { config } from './config';
 import MessageController from './src/controllers/message';
 import CommandInteractionController from './src/controllers/interaction/command';
+import ReactionController from './src/controllers/reaction';
 import EmojiService from './src/EmojiService';
+import { CacheType, CommandInteraction, MessageReaction, User } from 'discord.js';
+import ReplyableEvent from './src/ReplyableEvent';
+import ReactionEvent from './src/ReactionEvent';
 
 
 export const log = new Log('Bot');
@@ -23,5 +27,38 @@ client.on('messageCreate', async msg => {
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.guild) return;
-    if (interaction.isCommand()) return CommandInteractionController.handle(interaction);
+    if (interaction.isCommand()) return CommandInteractionController.handle(
+        new ReplyableEvent<CommandInteraction<CacheType>>({
+            guild: interaction.guild,
+            member: interaction.member,
+            event: interaction,
+            when: Date.now()
+        })
+    );
 })
+
+client.on('messageReactionAdd', async (reaction: MessageReaction, user: User) => {
+    if (!reaction.message.guild) return;
+    ReactionController.handle(
+        new ReactionEvent({
+            type: 'add',
+            event: reaction,
+            guild: reaction.message.guild,
+            member: await reaction.message.guild.members.fetch({ user }),
+            when: Date.now()
+        })
+    );
+});
+
+client.on('messageReactionRemove', async (reaction: MessageReaction, user: User) => {
+    if (!reaction.message.guild) return;
+    ReactionController.handle(
+        new ReactionEvent({
+            type: 'remove',
+            event: reaction,
+            guild: reaction.message.guild,
+            member: await reaction.message.guild.members.fetch({ user }),
+            when: Date.now()
+        })
+    );
+});

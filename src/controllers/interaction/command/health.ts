e2f8c4b4f2cc93dc68332e5446b08806b8d04006
anore <66754841+codelumen@users.noreply.github.com>
2022-07-16 03:44:28 +0300

    adapted to new event type, refactored

--- src/controllers/interaction/command/health.ts
+++ src/controllers/interaction/command/health.ts
@@ -1,34 +1,41 @@
-import { CacheType, CommandInteraction, Message } from "discord.js";
-import { messageWrapper } from '../../../utils/messageWrapper';
+import { CacheType, CommandInteraction } from "discord.js";
 import * as os from 'os-utils';
-import { stateEmoji } from "../../../utils/stateEmoji";
 import EmojiService from "../../../EmojiService";
 import { CommandController } from "../../../CommandController";
 import Permission from "../../../Permission";
 import health from "../../../structures/permissions/health";
+import ReplyableEvent from '../../../ReplyableEvent';
+import Embed from "../../../Embed";
 
 
 export class HealthController extends CommandController {
     public name = 'health';
     public perm: Permission = health;
 
-    public async callback(interaction: CommandInteraction<CacheType>) {
-        let latency = new Date().getTime() - interaction.createdTimestamp;
+    public async callback(interaction: ReplyableEvent<CommandInteraction<CacheType>>) {
+        let latency = interaction.when - interaction.event.createdTimestamp;
+
+        await interaction.event.deferReply()
+
         let cpuUsage = ((await new Promise(res => os.cpuUsage(res))) as number) * 100;
         let totalRAM = os.totalmem() as number;
         let usedRAM = totalRAM - os.freemem();
 
-        let latencyState = stateEmoji(latency / 1000, true);
-        let cpuState = stateEmoji(cpuUsage / 100, true);
-        let ramState = stateEmoji(usedRAM / totalRAM, true);
+        let latencyLoad = 1 - latency / 500;
+        let cpuStateLoad = 1 - cpuUsage / 100;
+        let ramStateLoad = 1 - usedRAM / totalRAM;
+        let totalLoad = (latencyLoad + cpuStateLoad + ramStateLoad) / 3;
+        let loadEmoji = totalLoad < 0.33 ?
+            EmojiService.insert('connection_stable') : (totalLoad < 0.66 ?
+                EmojiService.insert('connection_moderate') : EmojiService.insert('connection_bad'));
     
-        await interaction.reply(messageWrapper({
-            title: `${EmojiService.get('botstats')} Состояние сервера`,
-            text: 'Неполадки происходят в том случае, если большинство индикаторов желтые или красные. В таком случае информируйте администрацию сервера.',
+        await interaction.event.editReply(Embed.new({
+            title: `${loadEmoji} Состояние сервера`,
+            text: 'Если индикатор красный, то скорее всего происходят технические неполадки. В таком случае необходимо информировать разработчика.',
             fields: [
-                [ `${latencyState} Latency`, latency.toString() + 'ms', true ],
-                [ `${cpuState} CPU`, cpuUsage.toFixed(0) + '%', true ],
-                [ `${ramState} RAM`, `${ +usedRAM.toFixed(0) / 1000 }/${ +totalRAM.toFixed(0) / 1000 } GB`, true ]
+                [ `Latency`, latency.toString() + 'ms', true ],
+                [ `CPU`, cpuUsage.toFixed(0) + '%', true ],
+                [ `RAM`, `${ +usedRAM.toFixed(0) / 1000 }/${ +totalRAM.toFixed(0) / 1000 } GB`, true ]
             ]
         }));
     }
